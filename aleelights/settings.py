d8adaa07167f4dd73918f60101223eed789a02c4
ali0454hassan <ali0454hassan@gmail.com>
2026-03-08 21:58:16 +0500

    Fix: add os import, update ALLOWED_HOSTS for PythonAnywhere

--- aleelights/settings.py
+++ aleelights/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-9(($ndi6-!v!h+is^nhbt64=j2u%n43)mvul_q0pznkw_88m@k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['alihassan536.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
